da1556a7188b217ac7264331917202634f7942d6
Sahil Patil <71595574+sahilpatil9763@users.noreply.github.com>
2026-07-19 03:03:06 +0530

    Add Assignment 28 Q4: File copy program
    Implement a program that copies contents from one file to another. Includes the Python solution with file handling and error management, plus a sample input file (ABC.txt) for testing.

---
+++ Assignment_28/Assignment28_4.py
@@ -0,0 +1,32 @@
+"""
+Q4) Copy File Contents into Another File
+Problem Statement: Write a program which accepts two file names from the us
+    • First file is an existing file
+    • Second file is a new file
+Copy all contents from the first file into the second file.
+Input : ABC. txt Demo.txt
+"""
+
+def CopyFile(SourceFile, DestinatinoFile):
+    try:
+        sObj = open(SourceFile,"r")
+        dObj = open(DestinatinoFile,"w")
+
+        for lines in sObj:
+            dObj.write(lines)
+        
+        sObj.close()
+        dObj.close()
+
+        print("Content copied successfully")
+                    
+    except FileNotFoundError:
+        print("File is not present in current directory")
+
+def main():
+    Source = input("Enter existing file name : ")
+    Destination = input("Enter new file name : ")
+    CopyFile(Source, Destination)
+
+if __name__ == "__main__":
+    main()
